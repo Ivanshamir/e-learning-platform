a8547d5ce42548db6f3ab9bd17d364ed9b8e11c3
Ivanshamir <shamirimtiaz.ice@gmail.com>
2021-04-11 00:16:52 +0600

    add courses module

--- educa/settings.py
+++ educa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hc4=c3cku!-jurv+befxw+*_(5_yr90okyt@6h1+#c-mv32b5y'
+SECRET_KEY = 'django-insecure-9unkv)1zw4+%9q4o7q-px)@+11=n_if^xefxnq2^h-575ywja7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
